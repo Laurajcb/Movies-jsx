cc5bf727f2f001c394871db21351ddcef7d8f8b6
Laurajcb <callejaslaura96@gmail.com>
2022-09-20 23:20:18 -0500

    Implemet api with the  List componet

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, Navigate } from 'react-router-dom';
+import axios from 'axios';
 
- function List() {
+
+
+function List() {
   const token = localStorage.getItem('token');
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const endPoint = 'https://api.themoviedb.org/3/discover/movie?api_key=b463aec86cb8756bf7c98134a7166a7f&language=en-US&page=1'
+    axios.get(endPoint)
+      .then(response => {
+        const apiData = response.data;
+        setMovieList(apiData.results)
+      })
+  }, [setMovieList]);
+
 
   return (
     <>
       {!token && <Navigate to="/" />}
+
       <section className='row'>
-        <div className='col-3'>
-          <div className='card'>
-            <img src='...' className='card-img-top' alt='...' />
-            <div className='card-body'>
-              <h5 className='card-titel'>Card Title</h5>
-              <p className='card-tex'>Some quick text that will goes in the card description, just to make an idea of how is goint to look like.
-              </p>
-              <Link to='/' className='btn btn-primary'>
-                Go to the actuall Film
-              </Link>
-            </div>
-          </div>
-        </div>
+        {
+          movieList.map((oneMovie, idx) => {
+            return (
+              <div className='col-3' key={idx}>
+                <div className='card my-4'>
+                  <img src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`} className='card-img-top' alt='...' />
+                  <div className='card-body'>
+                    <h5 className='card-titel'>{oneMovie.title.substring(0, 20)}</h5>
+                    <p className='card-tex'>{oneMovie.overview.substring(0, 80)}...
+                    </p>
+                    <Link to='/' className='btn btn-primary'>
+                      View detail
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
       </section>
     </>
   )
